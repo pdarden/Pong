8ee39c7d39d48a729565d296f54e433cdd63b9fd
Parinda Darden <parinda.darden@gmail.com>
2013-11-16 14:06:26 -0500

    made red ball

--- pong.rb
+++ pong.rb
@@ -1,12 +1,25 @@
+require "bundler/setup"
 require "hasu"
+
+Hasu.load "ball.rb"
 
 class Pong < Hasu::Window
   WIDTH = 768
   HEIGHT = 576
 
+  # Width and height of the game's window
   def initialize
     super(WIDTH, HEIGHT, false)
   end
+
+  def reset
+    @ball = Ball.new
+  end
+
+  def draw
+    @ball.draw(self)
+  end
+
 end
 
 Pong.run
